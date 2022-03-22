9787860f272cfb3006923a688c14526c172272ef
Ântony Salvi <nysalvi20@gmail.com>
2022-03-22 14:58:46 -0300

    Add project files.

---
+++ ClubeLeitura.ConsoleApp/TelaCadastroAmigo.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeLeitura.ConsoleApp
+{
+    public class TelaCadastroAmigo
+    {
+        public Amigo[] amigos;
+        public int numeroAmigo;
+        public Notificador notificador;
+
+        public string MostrarOpcoes()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Cadastro de Amigos");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite 1 para Inserir");
+            Console.WriteLine("Digite 2 para Editar");
+            Console.WriteLine("Digite 3 para Excluir");
+            Console.WriteLine("Digite 4 para Visualizar");
+
+            Console.WriteLine("Digite s para sair");
+
+            string opcao = Console.ReadLine();
+
+            return opcao;
+        }
+
+        public void InserirNovoAmigo()
+        {
+            MostrarTitulo("Inserindo novo Amigo");
+
+            Amigo amigo = ObterAmigo();
+
+            amigo.numero = ++numeroAmigo;
+
+            int posicaoVazia = ObterPosicaoVazia();
+            amigos[posicaoVazia] = amigo;
+
+            notificador.ApresentarMensagem("Amigo inserido com sucesso!", "Sucesso");
+        }
+        private Amigo ObterAmigo()
+        {
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o nome do responsável: ");
+            string responsavel = Console.ReadLine();
+
+            Console.Write("Digite o telefone: ");
+            string telefone = Console.ReadLine();
+
+            Console.Write("Digite o endereço: ");
+            string endereço = Console.ReadLine();
+
+            Amigo amigo = new Amigo();
+
+            amigo.nome = nome;
+            amigo.nomeResponsavel = responsavel;
+            amigo.telefone = telefone;
+            amigo.endereco = endereço;
+            return amigo;
+        }
+        public void EditarAmigo()
+        {
+            MostrarTitulo("Editando Amigo");
+
+            VisualizarAmigos("Pesquisando");
+
+            Console.Write("Digite o número do amigo que deseja editar: ");
+            int numeroAmigo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < amigos.Length; i++)
+            {
+                if (amigos[i].numero == numeroAmigo)
+                {
+                    Amigo amigo = ObterAmigo();
+
+                    amigos[i].numero = numeroAmigo;
+                    amigos[i] = amigo;
+
+                    break;
+                }
+            }
+
+            notificador.ApresentarMensagem("Amigo editado com sucesso", "Sucesso");
+        }
+
+
+        public void ExcluirAmigo()
+        {
+            MostrarTitulo("Excluindo Amigo");
+
+            VisualizarAmigos("Pesquisando");
+
+            Console.Write("Digite o número do amigo que deseja excluir: ");
+            int numeroAmigo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < amigos.Length; i++)
+            {
+                if (amigos[i].numero == numeroAmigo)
+                {
+                    amigos[i] = null;
+                    break;
+                }
+            }
+
+            notificador.ApresentarMensagem("Amigo excluído com sucesso", "Sucesso");
+        }
+
+        public void MostrarTitulo(string titulo)
+        {
+            Console.Clear();
+
+            Console.WriteLine(titulo);
+
+            Console.WriteLine();
+        }
+
+        public void VisualizarAmigos(string tipo)
+        {
+            if (tipo == "Tela")
+                MostrarTitulo("Visualização de Revistas");
+
+            for (int i = 0; i < amigos.Length; i++)
+            {
+                if (amigos[i] == null)
+                    continue;
+
+                Amigo a = amigos[i];
+
+                Console.WriteLine("Número: " + a.numero);
+                Console.WriteLine("Nome: " + a.nome);
+                Console.WriteLine("Responsável: " + a.nomeResponsavel);
+                Console.WriteLine("Telefone: " + a.telefone);
+                Console.WriteLine("Endereço: " + a.endereco);
+                Console.WriteLine();
+            }
+        }
+
+        public int ObterPosicaoVazia()
+        {
+            for (int i = 0; i < amigos.Length; i++)
+            {
+                if (amigos[i] == null)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
